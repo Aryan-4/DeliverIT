0b61413dce987205ade6f1e3e73fd5b3f7397c4f
Aryan-4 <76207339+Aryan-4@users.noreply.github.com>
2023-07-23 00:48:39 -0400

    Delete UI/admin/js directory

--- UI/admin/js/signout_admin.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.signout').click(function() {
-        event.preventDefault()
-        localStorage.removeItem("token")
-        window.location.href = "./login.html";
-    })
-})
