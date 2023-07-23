f843bc7c6cc77bb2b79e5309ef31a0566a0d42bd
Aryan-4 <76207339+Aryan-4@users.noreply.github.com>
2023-07-23 00:36:24 -0400

    Delete UI/js/fetchscripts directory

--- UI/js/fetchscripts/logout.js
+++
@@ -1,8 +0,0 @@
-signout = document.getElementById("signout")
-signout.onclick = () => {
-    event.preventDefault()
-    if (localStorage.getItem("token") != null) {
-        localStorage.removeItem("token")
-        window.location.replace("./index.html");
-    }
-}
